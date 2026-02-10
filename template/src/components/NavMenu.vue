e8ed0e16cf25487a43de9f1e078b75922586a5ce
Bianca <biancamartinez0715@icloud.com>
2026-02-09 19:15:05 -0500

    update

--- template/src/components/NavMenu.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <nav class="flex justify-center gap-4"
-        <RouterLink
-            to="/"
-            class="hover:underline" hover:underline-offset-4 [&.router-link-active]:underline [&.router-link-active]:underline"
