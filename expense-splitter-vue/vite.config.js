8ac9613bf7c83eccb0385917cdf6caf65acfe7e9
beemtz0715-max <beemtz0715@gmail.com>
2026-03-12 11:17:29 -0400

    initial api layout

--- expense-splitter-vue/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-import { cloudflare } from "@cloudflare/vite-plugin";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), cloudflare()],
-})
